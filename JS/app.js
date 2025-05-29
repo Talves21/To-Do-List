c1a3f92c0b34b41f3e8701ece673eb3974fff50f
Terrell-Alves <terrell.alves@mscoding.org>
2025-05-29 12:26:56 -0500

    Finished Project
    Finished the javascript portion completing the project

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,44 @@
 let task = document.querySelector('#task');
 let addTask = document.querySelector('#addTask');
-let done = document.querySelector('#done');
-done.style.display = 'none';
+let complete = document.querySelector('#complete');
+let restart = document.querySelector('#restart');
+
 
 addTask.addEventListener('click', () => {
     let taskList = document.querySelector('.taskList');
     let list = document.createElement('li');
     let input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    input.classList.add('taskList');
+    input.setAttribute('type', 'text',);
+    input.setAttribute('placeholder', 'Enter a Task')
     taskList.appendChild(list);
     list.appendChild(input);
+
+    let addComplete = document.createElement('button');
+    addComplete.classList.add('ms-3');
+    addComplete.innerText ='Completed';
+    list.appendChild(addComplete);
+
+    addComplete.addEventListener('click', () => {
+        input.disabled = true;
+        addComplete.style.display = 'none';
+    })
+
+    restart.addEventListener('click', () => {
+        list.remove();
+    })
 })
 
-task.addEventListener('keydown', () => {
-    if (Event.key === 'Enter') {
-        task.disabled = true;
-    }
+complete.addEventListener('click', () => {
+    task.disabled = true;
+    complete.style.display = 'none';
+})
+
+edit.addEventListener('click', () => {
+    complete.style.display = 'inline';
+    task.disabled = false;
+})
+
+restart.addEventListener('click', () => {
+        task.disabled = false;
+        complete.style.display = 'inline';
 })
